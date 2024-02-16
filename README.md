17a0b417a2ece7dce7efbdfbbab1e5ad1ff4dd46
nikarcosta <mrc32467098@outlook.com>
2024-02-16 11:09:30 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# B04rdC4mp-V2 API Documentation
+
+Back-end for B04rdC4mp, a management system for a board game rental company
+
+Coming soon!
---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+const PORT = 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running at PORT = ${PORT}.`);
+});
