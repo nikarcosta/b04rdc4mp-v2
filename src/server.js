4f97b8ec34391b5ec9648b0ae2b5514bc6af6266
nikarcosta <mrc32467098@outlook.com>
2024-02-19 10:38:37 -0300

    feature: add database connection

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
   console.log(`Server running at PORT = ${PORT}.`);
