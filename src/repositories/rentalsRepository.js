43833ad57258ec303154affea442452bce7a2620
nikarcosta <mrc32467098@outlook.com>
2024-03-02 08:02:03 -0300

    fix: fix returned object of getRentalsRepository function

--- src/repositories/rentalsRepository.js
+++ src/repositories/rentalsRepository.js
@@ -2,9 +2,21 @@ import { db } from "../config/database.connection.js";
 
 export async function getRentalsRepository() {
   return db.query(
-    `SELECT * FROM rentals 
+    `SELECT 
+      rentals.id AS id,
+      rentals."customerId" AS customerId,
+      rentals."gameId" AS gameId,
+      rentals."rentDate" AS rentDate,
+      rentals."daysRented" AS daysRented,
+      rentals."returnDate" AS returnDate,
+      rentals."originalPrice" AS originalPrice,
+      rentals."delayFee" AS delayFee,
+      json_build_object('id', customers.id, 'name', customers.name) AS customer,
+      json_build_object('id', games.id, 'name', games.name) AS game
+    FROM rentals
     JOIN customers ON customers.id = rentals."customerId"
-    JOIN games ON games.id = rentals."gameId"`
+    JOIN games ON games.id = rentals."gameId"
+    `
   );
 }
 
